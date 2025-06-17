dde21c9f5e7a175a1764ad5c53f75c9c1394a420
Justin200219 <jubbelin@gmail.com>
2025-06-17 13:30:03 +0200

    Remove unused pages and update detection flow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
 import AIChat from "./components/AIChat";
 import LanguageDetection from "./components/LanguageDetection";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import NameInput from "./components/NameInput";
-import Welcome from "./components/Welcome";
-import WordsPage from "./components/WordsPage";
-import PronunciationPage from "./components/PronunciationPage";
-import SentencesPage from "./components/SentencesPage";
-import GamesPage from "./components/GamesPage";
+    window.location.href = "/Signup/name_input.html";
 
-import { LanguageProvider, useLanguage } from "./contexts/LanguageContext";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import "./App.css";
-
-function AppContent() {
-  const { t, handleLanguageDetected } = useLanguage();
-  const navigate = useNavigate();
 
   const handleDetected = (language) => {
     handleLanguageDetected(language);
